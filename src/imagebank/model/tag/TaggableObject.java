d0d37ed378aa42c295243b95cb46d8fc91248442
Maël Alet <noemail@fake.com>
2018-03-30 15:03:32 +0200

    Modifie l'implémentation de TaggableObject.
    Les méthodes qui ne doivent pas être utilisées par "l'utilisateur" des
    TaggableObject ont été mises "protected".  L'implémentation de
    hasTag() est maintenant récursive et permet donc de savoir si le
    TaggableObject contient un certain Tag ou si un Tag du TaggableObject
    contient ce tag, récursivement.

--- src/imagebank/model/tag/TaggableObject.java
+++ src/imagebank/model/tag/TaggableObject.java
@@ -1,28 +1,37 @@
 package imagebank.model.tag;
 
 import java.io.Serializable;
+import java.util.LinkedHashSet;
 
 public abstract class TaggableObject implements Taggable, Serializable {
 
-	private HashSet<Tag> tagSet;
+	private LinkedHashSet<Tag> tagSet;
 
 	protected TaggableObject() {
-		tagSet = new HashSet<>();
+		tagSet = new LinkedHashSet<>();
 	}
 
 	@Override
-	public boolean addTag(Tag tag) {
+	protected boolean addTag(Tag tag) {
 		return tagSet.add(tag);
 	}
 
 	@Override
-	public boolean remove(Tag tag) {
+	protected boolean remove(Tag tag) {
 		return tagSet.remove(tag);
 	}
 
 	@Override
 	public boolean hasTag(Tag tag) {
-		return tagSet.contains(tag);
+		if (tagSet.contains(tag))
+			return true;
+
+		for (Tag ownedTag : tagSet) {
+			if (ownedTag.hasTag(tag))
+				return true;
+		}
+
+		return false;
 	}
 
 	@Override
