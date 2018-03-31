c1306a6614dd67e421dec857c9c5d49232accd02
Maël Alet <noemail@fake.com>
2018-03-31 17:56:37 +0200

    Implémente les fonctions de rotation d'AVLTree.

--- src/imagebank/util/AVLTree.java
+++ src/imagebank/util/AVLTree.java
@@ -64,4 +64,86 @@ public class AVLTree<K,V> implements SortedMap<K,V> {
 			return a ^ b;
 		}
 	}
+
+	private static <K,V> Node<K,V> rotateRightRight(Node<K,V> root) {
+		Node<K,V> rootRightChild = root.rightChild;
+
+		root.rightChild = rootRightChild.leftChild;
+		rootRightChild.leftChild = root;
+
+		if (rootRightChild.balance == 0) {
+			root.balance = 1;
+			rootRightChild.balance = -1;
+		} else {
+			root.balance = 0;
+			rootRightChild.balance = 0;
+		}
+
+		return rootRightChild;
+	}
+
+	private static <K,V> Node<K,V> rotateLeftLeft(Node<K,V> root) {
+		Node<K,V> rootLeftChild = root.leftChild;
+
+		root.leftChild = rootLeftChild.rightChild;
+		rootLeftChild.rightChild = root;
+
+		if (rootLeftChild.balance == 0) {
+			root.balance = -1;
+			rootLeftChild.balance = 1;
+		} else {
+			root.balance = 0;
+			rootLeftChild.balance = 0;
+		}
+
+		return rootLeftChild;
+	}
+
+	private static <K,V> Node<K,V> rotateLeftRight(Node<K,V> root) {
+		Node<K,V> rootLeftChild = root.leftChild;
+		Node<K,V> rootRightLeftChild = rootLeftChild.rightChild;
+
+		root.leftChild = rootRightLeftChild.rightChild;
+		rootLeftChild.rightChild = rootRightLeftChild.leftChild;
+		rootRightLeftChild.rightChild = root;
+		rootRightLetChild.leftChild = rootLeftChild;
+
+		if (rootRightLeftChild.balance == 1) {
+			root.balance = 0;
+			rootLeftChild = -1;
+		} else if (rootRightLeftChild.balance == 0) {
+			root.balance = 0;
+			rootLeftChild = 0;
+		} else {
+			root.balance = 1;
+			rootLeftChild = 0;
+		}
+		rootRightLeftChild.balance = 0;
+
+		return rootRightLeftChild;
+	}
+
+	private static <K,V> Node<K,V> rotateRightLeft(Node<K,V> root) {
+		Node<K,V> rootRightChild = root.rightChild;
+		Node<K,V> rootLeftRightChild = rootRightChild.leftChild;
+
+		root.rightChild = rootLeftRightChild.leftChild;
+		rootRightChild.leftChild = rootLeftRightChild.rightChild;
+		rootLeftRightChild.leftChild = root;
+		rootLeftRightChild.rightChild = rootRightChild;
+
+		if (rootLeftRightChild.balance == 1) {
+			root.balance = -1;
+			rootRightChild.balance = 0;
+		} else if (rootLeftRightChild.balance == 0) {
+			root.balance = 0;
+			rootRightChild.balance = 0;
+		} else {
+			root.balance = 0;
+			rootRightChild.balance = 1;
+		}
+		rootLeftRightChild.balance = 0;
+
+		return rootLeftRightChild;
+	}
 }
