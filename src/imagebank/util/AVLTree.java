c3ee846edc5a70b6178a16172a2ddf44a8d9e971
Maël Alet <noemail@fake.com>
2018-04-04 15:38:51 +0200

    Utilise la classe AbstractMap dans AVLTree.
    Utiliser les classes AbstractMap et AbstractMap.SimpleEntry permet
    d'éviter d'avoir à réécrire du code qui ne change pas suivant
    l'implémentation de l'interface Map.

--- src/imagebank/util/AVLTree.java
+++ src/imagebank/util/AVLTree.java
@@ -2,73 +2,25 @@ package imagebank.util;
 
 import java.util.SortedMap;
 import java.util.Map;
+import java.util.AbstractMap;
 import java.util.Deque;
 import java.util.ArrayDeque;
 
-public class AVLTree<K,V> implements SortedMap<K,V> {
+public class AVLTree<K,V> extends AbstractMap<K,V>
+				implements SortedMap<K,V>, Serializable {
 
-	public static class Node<K,V> implements Map.Entry<K,V>  {
-		private K key;
-		private V value;
+	public static class Node<K,V> extends AbstractMap.SimpleEntry<K,V> {
 		protected char balance;
 		protected Node<K,V> leftChild;
 		protected Node<K,V> rightChild;
 
 		public Node(K key, V value) {
-			this.key = key;
-			this.value = value;
+			super(key, value);
 			this.balance = 0;
 			this.leftChild = null;
 			this.rightChild = null;
 		}
 
-		@Override
-		public K getKey() {
-			return key;
-		}
-
-		@Override
-		public V setValue(V value) {
-			V oldValue = this.value;
-			this.value = value;
-			return oldValue;
-		}
-
-		@Override
-		public V getValue() {
-			return value;
-		}
-
-		@Override
-		public boolean equals(Object other) {
-			if (!(other instanceof Node))
-				return false;
-
-			Node<?,?> o = (Node<?,?>) other;
-
-			boolean keysEquals;
-			if (this.getKey() == null)
-				keysEquals = o.getKey() == null;
-			else
-				keysEquals = this.getKey().equals(o.getKey());
-
-			boolean valuesEquals;
-			if (this.getValue() == null) {
-				valuesEquals = o.getValue() == null;
-			} else {
-				valuesEquals = this.getValue()
-							.equals(o.getValue());
-			}
-
-			return keysEquals && valuesEquals;
-		}
-
-		@Override
-		public int hashCode() {
-			int a = getKey() == null ? 0 : getKey().hashCode();
-			int b = getValue() == null ? 0 : getValue().hashCode();
-			return a ^ b;
-		}
 	}
 
 	@Override
