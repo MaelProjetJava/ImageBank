3ca6c8041ae77f1912d14fff507f8df9f6e5597b
Maël Alet <noemail@fake.com>
2018-04-18 18:13:16 +0200

    Termine d'implémenter la classe Tag.
    Ajoute et implémente les méthodes 'protected' nécessaires pour
    implémenter le Tagger.
    
    Modifie le constructeur de la version avec valeur de la classe Tag,
    auparavant les Tags avec le même nom mais des valeurs différentes
    possédaient tous une instance différente de la version "unvalued" du
    Tag. Dorénavant, l'unique instance est passée en argument du
    constructeur par le Tagger.

--- src/imagebank/model/tag/Tag.java
+++ src/imagebank/model/tag/Tag.java
@@ -1,10 +1,17 @@
 package imagebank.model.tag;
 
+import java.util.Deque;
+
+import imagebank.util.AVLTree;
+import static imagebank.util.AVLTree.*;
+import imagebank.model.Image;
+
 public class Tag extends TaggableObject {
 
 	private String name;
 	private Long value;
 	private Tag unvaluedTagVersion;
+	private Node<Long,Tag> valuesTreeRoot;
 	private AVLTree<String,Image> taggedImages;
 
 	protected Tag(String name) {
@@ -12,13 +19,15 @@ public class Tag extends TaggableObject {
 		this.name = name;
 		this.value = null;
 		this.unvaluedTagVersion = this;
+		this.valuesTreeRoot = null;
+		this.taggedImages = new AVLTree<>();
 	}
 
-	protected Tag(String name, long value) {
-		super();
-		this.name = name;
+	protected Tag(String name, long value, Tag unvaluedTagVersion) {
+		this(name);
 		this.value = value;
-		this.unvaluedTagVersion = new Tag(name);
+		this.unvaluedTagVersion = unvaluedTagVersion;
+		this.valuesTreeRoot = new Node<>(value, this);
 	}
 
 	public String getName() {
@@ -36,8 +45,48 @@ public class Tag extends TaggableObject {
 		return unvaluedTagVersion;
 	}
 
-	public Iterable<Image> getTaggedImages() {
+	protected Tag getValuedTag(long value) throws UnvaluedTagException {
+		if (!hasValue())
+			throw new UnvaluedTagException();
 
+		Node<Long,Tag> foundNode = search(valuesTreeRoot, value);
+		return foundNode != null ? foundNode.getValue() : null;
+	}
+
+	protected Node<Long,Tag> addValuedTag(Tag valuedTag)
+						throws UnvaluedTagException {
+		if (!hasValue() || !valuedTag.hasValue())
+			throw new UnvaluedTagException();
+
+		Node<Long,Tag> newNode = valuedTag.valuesTreeRoot;
+		Deque<PathStep<Node<Long,Tag>>> insertPath
+					= buildPath(valuesTreeRoot, newNode);
+
+		return insert(insertPath, newNode);
+	}
+
+	protected Node<Long,Tag> removeValuedTag(Tag valuedTag)
+						throws UnvaluedTagException {
+		if (!hasValue() || !valuedTag.hasValue())
+			throw new UnvaluedTagException();
+
+		Deque<PathStep<Node<Long,Tag>>> deletePath
+				= buildPath(valuesTreeRoot,
+					valuedTag.valuesTreeRoot.getKey());
+
+		return delete(deletePath, valuesTreeRoot);
+	}
+
+	protected void addTaggedImage(Image image) {
+		taggedImages.put(image.getName(), image);
+	}
+
+	protected void removeTaggedImage(Image image) {
+		taggedImages.remove(image.getName());
+	}
+
+	public Iterable<Image> getTaggedImages() {
+		return taggedImages.values();
 	}
 
 	public boolean hasValue() {
