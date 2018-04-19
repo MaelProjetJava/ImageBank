164b08b7b637b8cf67bddd4e25f01c810b5f6680
Maël Alet <noemail@fake.com>
2018-04-19 13:00:12 +0200

    Tagger.java: implémente les méthodes getTag(...).

--- src/imagebank/model/tag/Tagger.java
+++ src/imagebank/model/tag/Tagger.java
@@ -23,12 +23,36 @@ public class Tagger implements Serializable {
 	}
 
 	public static Tag getTag(String name) {
+		Tag tag = getInstance().tags.get(name);
 
+		if (tag == null) {
+			tag = new Tag(name);
+			getInstance().tags.put(name, tag);
+		}
+
+		return tag.getUnvaluedTag();
 	}
 
 	public static Tag getTag(String name, long value)
 						throws UnvaluedTagException {
+		Tag rootTag = getInstance().tags.get(name);
 
+		if (rootTag == null) {
+			Tag unvaluedVersion = new Tag(name);
+			rootTag = new Tag(name, value, unvaluedVersion);
+			getInstance().tags.put(name, rootTag);
+			return rootTag;
+		}
+
+		Tag tag = rootTag.getValuedTag(value);
+		if (tag != null)
+			return tag;
+
+		tag = new Tag(name, value, rootTag.getUnvaluedTag());
+		rootTag = rootTag.addValuedTag(tag);
+		getInstance().tags.put(name, rootTag);
+
+		return tag;
 	}
 
 	public static void tag(Image image, Tag tag) {
